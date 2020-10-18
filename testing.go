43298dc1e2e7bf5507b05ae9339912dc09deb1ec
SasoriOfTheRedSand <vikram01kmr@gmail.com>
2020-10-19 02:15:50 +0530

    testing done.

--- testing.go
+++ testing.go
@@ -68,6 +68,48 @@ func TestGetMeetingIDNotFound(t *testing.T) {
 	}
 }
 
+func TestListMeetingTimeFrame(t *testing.T) {
+	req, err := http.NewRequest("GET", "/meeting/{start_time}{end_time}", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	req.URL.RawQuery = q.Encode()
+  q.Add("start_time", "06/05/2020 11:00PM")
+  q.ADD("end_time", "08/09/2020 11:00PM")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ListMeetingTimeFrame)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"_id":1,"_id":2,"_id":15,"_id":154,"_id":41,"_id":152}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestListMeetingTimeFrameNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/meeting", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+  q.Add("start_time", "06/05/2020 11:00PM")
+  q.ADD("end_time", "08/09/2020 11:00PM")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ListMeetingTimeFrame)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status == http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+}
 
 
 func TestListMeetingParticipant(t *testing.T) {
@@ -95,18 +137,18 @@ func TestListMeetingParticipant(t *testing.T) {
 }
 
 func TestListMeetingParticipantNotFound(t *testing.T) {
-	req, err := http.NewRequest("GET", "/meeting", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	q := req.URL.Query()
-	q.Add("email", "hekkkksksk@gmail.com")
-	req.URL.RawQuery = q.Encode()
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(ListMeetingParticipant)
-	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status == http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusBadRequest)
-	}
+  req, err := http.NewRequest("GET", "/meeting", nil)
+  if err != nil {
+    t.Fatal(err)
+  }
+  q := req.URL.Query()
+  q.Add("email", "hekkkksksk@gmail.com")
+  req.URL.RawQuery = q.Encode()
+  rr := httptest.NewRecorder()
+  handler := http.HandlerFunc(ListMeetingParticipant)
+  handler.ServeHTTP(rr, req)
+  if status := rr.Code; status == http.StatusOK {
+    t.Errorf("handler returned wrong status code: got %v want %v",
+      status, http.StatusBadRequest)
+  }
 }
